7af4547249821a9c2f49f3238a4642a669705140
Alvarg27 <alvargbesne@gmail.com>
2023-01-30 18:26:09 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    unoptimized: true,
+    minimumCacheTTL: 60 * 60 * 24 * 365,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
